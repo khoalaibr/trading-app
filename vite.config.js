fb619931be59f32b7d37d64456ba27b2d2a1a20d
Ricardo <theartisanmidia@gmail.com>
2024-11-22 08:33:58 -0300

    Cambio en vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/market-app/', 
   build: {
     outDir: 'docs', // Asegúrate de que sea "docs"
   },
